ee2d46ed14f3b0c126ef8f1d42a65a6ef05d8ac1
Henrique Melo <hemelo@pm.me>
2025-09-19 22:50:52 -0300

    Add IRecordStorage interface for record management
    Introduced the `IRecordStorage` interface in the `Hemelo.Database.Core.Interfaces` namespace to manage variable-size records of data.

---
+++ Hemelo.Database.Core/Interfaces/IRecordStorage.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hemelo.Database.Core.Interfaces
+{
+    /// <summary>
+    /// High-Level layer for managing variable-size records of data.
+    /// A record can span multiple blocks, and the system handles the complexity of linking these blocks together.
+    /// </summary>
+    public interface IRecordStorage
+    {
+        uint CreateRecord();
+
+        uint CreateRecord(byte[] data);
+
+
+    }
+}
