2dac9f5101943829f2a983f07274a8329928f904
Henrique Melo <hemelo@pm.me>
2025-09-19 22:50:25 -0300

    Add IBlockStorage interface for block management
    Introduced a new `IBlockStorage` interface in the `Hemelo.Database.Core.Interfaces` namespace to manage fixed-size blocks of data. The interface provides properties for block content size, header size, and total size, along with methods to find and allocate blocks. XML documentation was added to describe the interface and its members.

---
+++ Hemelo.Database.Core/Interfaces/IBlockStorage.cs
@@ -0,0 +1,28 @@
+﻿namespace Hemelo.Database.Core.Interfaces
+{
+    /// <summary>
+    /// Low-Level layer for managing fixed-size blocks of data.
+    /// The block size is configurable, allowing for flexibility in storage management.
+    /// </summary>
+    public interface IBlockStorage
+    {
+        /// <summary>
+        /// Number of bytes of custom data that can be stored in a block.
+        /// </summary>
+        int BlockContentSize { get; }
+
+        /// <summary>
+        /// Number of bytes of header data that is stored in a block.
+        /// </summary>
+        int BlockHeaderSize { get; }
+
+        /// <summary>
+        /// Gets the total size, in bytes, of a block including its header and content.
+        /// </summary>
+        int BlockSize => BlockHeaderSize + BlockContentSize;
+
+        IBlock FindBlock(uint blockId);
+
+        IBlock AllocateBlock();
+    }
+}
