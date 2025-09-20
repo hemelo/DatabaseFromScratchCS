9dbc8aa0319f570b8f09112717bfc876458173fb
Henrique Melo <hemelo@pm.me>
2025-09-19 22:50:03 -0300

    Refactor: Move IBlock to Interfaces namespace
    The `IBlock` interface was moved from the `Hemelo.Database.Core`
    namespace to the `Hemelo.Database.Core.Interfaces` namespace.
    This change improves code organization by grouping interfaces
    under a dedicated `Interfaces` namespace.

--- Hemelo.Database.Core/Interfaces/IBlock.cs
+++ Hemelo.Database.Core/Interfaces/IBlock.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Hemelo.Database.Core
+namespace Hemelo.Database.Core.Interfaces
 {
     public interface IBlock
     {
