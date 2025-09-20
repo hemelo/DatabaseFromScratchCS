ca3f41c89dfd223f22e3c34eade1328a7d7a0258
Henrique Melo <hemelo@pm.me>
2025-09-19 22:51:21 -0300

    Add generic IIndex<K, V> interface for key-value indexing
    A new `IIndex<K, V>` interface has been introduced in the
    `Hemelo.Database.Core.Interfaces` namespace. This interface
    supports generic key-value indexing with methods for inserting,
    finding, and deleting key-value pairs. It includes functionality
    to find pairs based on comparison operations (e.g., larger than,
    smaller than) and supports custom value comparison for deletion.
    
    The interface is designed to be flexible, leveraging generic
    types for both keys and values. Necessary namespaces such as
    `System.Collections.Generic` and others have been imported to
    enable its functionality.

---
+++ Hemelo.Database.Core/Interfaces/IIndex.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hemelo.Database.Core.Interfaces
+{
+    public interface IIndex<K, V>
+    {
+        void Insert(K key, V value);
+
+        Tuple<K, V>? Find(K key);
+
+        IEnumerable<Tuple<K, V>> FindLargerThan(K key);
+
+        IEnumerable<Tuple<K, V>> FindLargerThanOrEqual(K key);
+
+        IEnumerable<Tuple<K, V>> FindSmallerThan(K key);
+
+        IEnumerable<Tuple<K, V>> FindSmallerThanOrEqual(K key);
+
+        void Delete(K key);
+
+        bool Delete(K key, V value, IComparer<V> valueComparer);
+    }
+}
